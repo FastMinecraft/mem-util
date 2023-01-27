f28cff0c2d4da5bdecd6d4e21aece1ad2fda6a83
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-01-27 16:26:47 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.fastmc.dev/")
+    }
+
+    val kotlinVersion: String by settings
+
+    plugins {
+        id("org.jetbrains.kotlin.jvm").version(kotlinVersion)
+    }
+}
