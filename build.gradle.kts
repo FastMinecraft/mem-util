8a9c414cc906c1a39fe0e8b09de0acc58657e039
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-01-28 03:10:10 -0500

    Setup Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("dev.fastmc.maven-repo").version("1.0-SNAPSHOT")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+    withSourcesJar()
+}
+
+kotlin {
+    val jvmArgs = mutableSetOf<String>()
+    (rootProject.findProperty("kotlin.daemon.jvm.options") as? String)
+        ?.split("\\s+".toRegex())?.toCollection(jvmArgs)
+    System.getProperty("gradle.kotlin.daemon.jvm.options")
+        ?.split("\\s+".toRegex())?.toCollection(jvmArgs)
+    kotlinDaemonJvmArgs = jvmArgs.toList()
+}
+
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    test {
+        javaToolchains {
+            this@test.javaLauncher.set(launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(8))
+                @Suppress("UnstableApiUsage")
+                vendor.set(JvmVendorSpec.IBM_SEMERU)
+            })
+        }
+        useJUnitPlatform()
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += listOf(
+                "-Xlambdas=indy",
+                "-Xjvm-default=all",
+                "-Xbackend-threads=0"
+            )
+        }
+    }
 }
